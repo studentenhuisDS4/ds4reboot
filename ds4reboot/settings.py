325acaba87cdf5a62dccf8b527734ef5e5a946fa
damienallen <contact@dallen.co>
2016-02-28 03:01:06 +0100

    Home page formatting

--- ds4reboot/settings.py
+++ ds4reboot/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'user.apps.UserConfig',
     'bierlijst.apps.BierlijstConfig',
     'eetlijst.apps.EetlijstConfig',
@@ -129,3 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
--- home/views.py
+++ home/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the main index.")
+    return render(request, 'home/index.html')
