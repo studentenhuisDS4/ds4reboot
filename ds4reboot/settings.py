2b2639f8b1ac814914f548ed05e70661d52487da
damienallen <contact@dallen.co>
2016-02-29 18:14:28 +0100

    Added base app with global templating

--- ds4reboot/settings.py
+++ ds4reboot/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'base.apps.BaseConfig',
     'home.apps.HomeConfig',
     'user.apps.UserConfig',
     'bierlijst.apps.BierlijstConfig',
