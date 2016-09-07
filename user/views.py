fdfa70e9ae1aa5b99ad4d427d68ea42c8f9044b8
damienallen <contact@dallen.co>
2016-09-07 02:15:36 +0200

    Case insensitive usernames

--- user/views.py
+++ user/views.py
@@ -53,7 +53,7 @@ def login_user(request):
     if request.method == 'POST':
 
         # get credentials from post and authenticate user
-        username = request.POST['username']
+        username = request.POST['username'].lower()
         password = request.POST['password']
 
         user = authenticate(username=username, password=password)
