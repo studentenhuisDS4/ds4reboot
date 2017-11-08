eee29c43608a2c828fde35dd8cdca8fe9027c260
David <davidzwa@gmail.com>
2017-11-08 14:16:58 +0100

    And home page (quick access update).

--- static/js/base.js
+++ static/js/base.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
         }
     });
 
-    if (window.location.pathname === "/bierlijst/") {
+    if (window.location.pathname === "/bierlijst/" || window.location.pathname === "/") {
        update_medals();
     }
 
