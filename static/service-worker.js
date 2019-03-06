4e1bd9b6632f345b3b2b9b553355006712a11330
David <davidzwa@gmail.com>
2019-03-06 22:48:16 +0100

    Revamp icons

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,8 +1,10 @@
-var asset_base = '../';
+var CACHE_NAME = 'ds4-cache-v1';
+
 self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open('ds4App').then(function (cache) {
+        caches.open(CACHE_NAME).then(function (cache) {
             return cache.addAll([
+                '/',
                 '/static/favicon.ico',
                 '/static/js/uikit.min.js',
                 '/static/js/base.js',
@@ -11,7 +13,6 @@ self.addEventListener('install', function (e) {
                 '/static/js/components/notify.min.js',
                 '/static/js/components/datepicker.min.js',
                 '/static/js/components/tooltip.min.js',
-
                 '/static/css/uikit.almost-flat.min.css',
                 '/static/css/components/notify.almost-flat.min.css',
                 '/static/css/components/datepicker.almost-flat.min.css',
@@ -28,16 +29,36 @@ self.addEventListener('install', function (e) {
 });
 
 // when the browser fetches a url
-self.addEventListener('fetch', function (event) {
-    // either respond with the cached object or go ahead and fetch the actual url
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            if (response) {
-                // retrieve from cache
-                return response;
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
             }
-            // fetch as normal
-            return fetch(event.request);
-        })
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
     );
 });
