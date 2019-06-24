49f8309cf6cb0166c28113f242e597e9b39169eb
David <davidzwa@gmail.com>
2019-06-24 23:04:13 +0200

    Tried to implement animation.

--- app/src/app/dinner-list/dinner-list.component.ts
+++ app/src/app/dinner-list/dinner-list.component.ts
@@ -22,12 +22,23 @@ import {IDinnerDate} from '../models/dinner.models';
             })),
             transition('in => out', animate('150ms ease-in-out')),
             transition('out => in', animate('200ms ease-in-out'))
+        ]),
+        trigger('slideOpen', [
+            state('1132', style({
+                'margin-bottom': '0px',
+            })),
+            state('*', style({
+                'margin-bottom': '-50px',
+            })),
+            transition('* => 1132', animate('150ms ease-in-out')),
+            transition('1132 => *', animate('200ms ease-in-out'))
         ])
     ]
 })
 export class DinnerListComponent implements OnInit {
     dinnersWeek: IDinnerDate[] = [];
-    helpMenuOpen = 'in';
+    weekCollapse = 'in';
+    dayCollapse = '1132';
 
     constructor(private dinnerListService: DinnerListService) {
         this.loadDinnerWeek();
@@ -37,7 +48,12 @@ export class DinnerListComponent implements OnInit {
     }
 
     toggleHelpMenu(): void {
-        this.helpMenuOpen = this.helpMenuOpen === 'out' ? 'in' : 'out';
+        this.weekCollapse = this.weekCollapse === 'out' ? 'in' : 'out';
+    }
+
+    openDinner(dinner: IDinnerDate): void {
+        this.dayCollapse = dinner.id.toString();
+        console.table(this.dayCollapse);
     }
 
     loadDinnerWeek() {
