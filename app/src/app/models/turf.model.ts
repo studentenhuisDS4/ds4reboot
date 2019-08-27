6af61fd2bc0a84896f8d8cccce35d7ad0d4f6fb7
david-beast <davidzwa@gmail.com>
2019-08-27 22:05:02 +0200

    Prevent schema error, add filter and try out decorator.

--- app/src/app/models/turf.model.ts
+++ app/src/app/models/turf.model.ts
@@ -1,5 +1,4 @@
-import {IHousemate} from './user.model';
-import {IResult} from './api.model';
+import {Filter} from './filter.model';
 
 export interface ITurfItem {
     turf_count: number;
@@ -12,4 +11,55 @@ export enum TurfType {
     BEER = 'beer',
     RWINE = 'red-wine',
     WWINE = 'white-wine'
+}
+
+export interface ITurfLogEntry {
+    id: number;
+    turf_to: string;
+    torf_by: string;
+    turf_user_id: number;
+    turf_note: string;
+    turf_count: number;
+    turf_time: Date;
+}
+
+function f() {
+    console.log('f(): evaluated');
+    return function(target, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log('f(): called');
+    };
+}
+
+function g() {
+    console.log('g(): evaluated');
+    return function(target, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log('g(): called');
+    };
+}
+
+export class TurfLogFilter implements Filter {
+    @f()
+    @g()
+    count?: number = null;
+    userTo?: string = null;
+    userNy?: string = null;
+    date?: Date;
+
+    serialize(): string {
+        let query = '';
+        Object.keys(this).forEach(key => {
+            if (this[key]) {
+                query += this[key] + '&';
+            }
+        });
+        return `?${query}`;
+
+    }
+
+}
+
+export interface ITurfLogAggregation {
+    aggregate_hours?: boolean;
+    aggregate_days?: boolean;
+    aggregate_since?: Date;
 }
