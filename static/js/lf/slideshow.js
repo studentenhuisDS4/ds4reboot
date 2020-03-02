7f3e4261ab8e5df1ce41c74e93f2f147f6b4a06c
David Zwart <davidzwa@gmail.com>
2020-03-02 18:55:37 +0100

    Change pic for wiki

--- static/js/lf/slideshow.js
+++ static/js/lf/slideshow.js
@@ -27,5 +27,5 @@ function showSlides() {
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
