babce2ca2726103245891017eab41e421083617d
David Zwart <davidzwa@gmail.com>
2020-04-16 16:48:32 +0200

    Add context to schema for auto user_id

--- user/api/api_snake_highscore.py
+++ user/api/api_snake_highscore.py
@@ -1,20 +1,29 @@
 from user.models import SnakeHighScore
 from user.api.serializers.snake_highscore import SnakeHighScoreSchema
-from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet
 
-from ds4reboot.api.utils import IsSuperUser
+from ds4reboot.api.utils import IsSuperUser, illegal_action, success_action
 
-class SnakeHighScoreViewSet(mixins.RetrieveModelMixin,
-                  mixins.CreateModelMixin,
-                  mixins.ListModelMixin,
+class SnakeHighScoreViewSet(RetrieveModelMixin,
+                  ListModelMixin,
                   GenericViewSet):
     queryset = SnakeHighScore.objects.all()
     serializer_class = SnakeHighScoreSchema
     filter_fields = '__all__'
 
+    @action(detail=False, methods=['post'])
+    def add(self, request):
+        serializer = SnakeHighScoreSchema(data=request.data, context={'user_id': request.user.id})
+        if serializer.is_valid():
+            serializer.save()
+            return success_action(serializer.data)
+        else:
+            return illegal_action(serializer.errors)
+
 class SnakeHighScoreAdminViewSet(
-                    mixins.DestroyModelMixin, 
+                    DestroyModelMixin, 
                     GenericViewSet):
     queryset = SnakeHighScore.objects.all()
     serializer_class = SnakeHighScoreSchema
--- user/api/serializers/snake_highscore.py
+++ user/api/serializers/snake_highscore.py
@@ -5,16 +5,27 @@ from marshmallow.validate import Length
 from rest_framework.exceptions import ValidationError
 from rest_marshmallow import Schema
 
+from marshmallow.validate import NoneOf
 from ds4reboot.api.utils import Map
+from ds4reboot.api.validators import ModelAttributeValidator
 from ds4reboot.api.validators import UniqueModelValidator
 from user.models import SnakeHighScore, SNAKE_NICK_LENGTH
 
 class SnakeHighScoreSchema(Schema):
     id = fields.Int(dump_only=True)
-    user_id = fields.Int(required=True, validate=[UniqueModelValidator(type=User, error="This user does not exist")])
+    user_id = fields.Int(required=True, validate=[ModelAttributeValidator(type=User, filter='id',
+                                                                          attribute='is_active')])
     nickname = fields.Str(required=True, validate=[Length(min=2, max=SNAKE_NICK_LENGTH)])
     score = fields.Int(required=True)
     time = fields.DateTime(dump_only=True)
+
+    @pre_load
+    def validate_user(self, data, **kwargs):
+        if "user_id" in self.context:
+            data["user_id"] = self.context["user_id"]
+        else:
+            raise ValueError("The user_id value was not specified in the Schema context.")
+        return data
     
     def create(self, validated_data):
         try:
