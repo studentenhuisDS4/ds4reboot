513daf7761af0f5f96a62095e7888b1ca04c8265
David Zwart <davidzwa@gmail.com>
2020-06-18 23:26:54 +0200

    Fix CI

--- .circleci/secret_settings.py
+++ .circleci/secret_settings.py
@@ -11,3 +11,14 @@ DATABASES = {
 SECRET_APPS = []
 SECRET_KEY = "ao#f$k))2#crl(qsfm796u!0mlicbrjh!$ucv9b8t6!sf&&a=l"
 DEBUG = True
+
+EMAIL_USE_SSL = True
+EMAIL_HOST = "fake.fake.fake"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "fake@fake.fake"
+EMAIL_HOST_PASSWORD = "fake.fake.fake"
+DEFAULT_FROM_EMAIL = 'fake.fake.fake'
+
+# Forward is a special API connection
+EMAIL_FORWARD_HOST_USER = "fake@fake.fake"
+EMAIL_FORWARD_HOST_PASSWORD = "fake.fake.fake"
